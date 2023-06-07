0c2220ec0130858bcc2d736722ce32e39ec46827
christopher mignon <dspmosfet@gmail.com>
2023-06-06 20:00:06 -0400

    update test

---
+++ my site/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+  <h1 my first php page </h1>
+
+  <?php
+  $x = "Hello World! 1";
+  $y = " Hello World 2";
+  echo $x;
+  echo "<br>";
+  echo $y;
+  ?>
+
+
+
+</body>
+
+</html>
---
+++ my site/index1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+  <h1 my first php page </h1>
+
+  <?php
+  echo "hello world!";
+  ?>
+
+
+
+</body>
+
+</html>
